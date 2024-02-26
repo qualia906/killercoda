57a70f6225e3780f5f0577826c2420c4c13e1b5d
Masato Koga <koga.masato@trainocate.com>
2024-02-26 10:46:36 +0000

    Update verify.sh

--- introduction-to-docker/scenario1/step14/verify.sh
+++ introduction-to-docker/scenario1/step14/verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Verify that the container image nginx:1.14-alpine was pulled and isavailable locally.
-docker images | grep nginx:1.14-alpine
+docker images | grep nginx | grep 1.14-alpine
 if [ $? -eq 0 ]; then
   exit 0
 fi

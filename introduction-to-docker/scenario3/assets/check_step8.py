a2ed86c4dd593f7c10da3b2438b62c5bd976802f
Masato Koga <koga.masato@trainocate.com>
2024-02-29 11:54:56 +0000

    Update check_step8.py

--- introduction-to-docker/scenario3/assets/check_step8.py
+++ introduction-to-docker/scenario3/assets/check_step8.py
@@ -7,10 +7,6 @@ def check_image_config(image_name):
     result = subprocess.run(['docker', 'inspect', image_name], capture_output=True, text=True)
     details = json.loads(result.stdout)
 
-    # Check image name and version
-    if 'python:3.6' not in details[0]['Config']['Image']:
-        return False
-
     # Check working directory
     if details[0]['Config']['WorkingDir'] != '/opt':
         return False

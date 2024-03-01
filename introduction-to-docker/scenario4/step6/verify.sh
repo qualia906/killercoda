6fb62098c5bb029ebcb7d797fe220f8da672a7f6
Masato Koga <koga.masato@trainocate.com>
2024-03-01 11:46:35 +0000

    Modify step6/verify.sh

--- introduction-to-docker/scenario4/step6/verify.sh
+++ introduction-to-docker/scenario4/step6/verify.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 現在ローカルに存在するすべてのDockerイメージのリストを取得し、Pythonスクリプトに渡す
-docker images --format "{{.Repository}}:{{.Tag}}" | python3 check_step6.py
+docker images --format "{{.Repository}}:{{.Tag}}" | python3 /my/location/check_step6.py
